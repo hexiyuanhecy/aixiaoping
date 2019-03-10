0a553716ff29d6924052b2436c78bac6c5a0140f
hexiyuanhecy <hexiyuanhecy@gmail.com>
2019-03-10 22:48:10 +0800

    小半成品的图标布局提交，我的脖子则特么痛！！

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/header/Header'
+import HomeIcons from './components/Icons'
 export default {
   name: 'Home',
   components:{
-    HomeHeader
+    HomeHeader,
+    HomeIcons
   }
 }
 </script>
